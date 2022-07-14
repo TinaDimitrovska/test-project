e1ab2738781c4977b268abeb1596157ec336c5cd
Tina <hristinadimitrovska@hotmail.com>
2022-07-14 17:27:06 +0200

    commit changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        // $users = User::all();
-        // return view('user.index-user', ['users' => $users]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 
 
